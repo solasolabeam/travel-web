15060c983083edcc060cb73cf97e1064c9a35fc9
sol <nathf1005@naver.com>
2024-08-22 20:47:37 +0900

    StrictMode 복구

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
-      <App />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </Provider>
   </QueryClientProvider>
 );
