9eaf81d33f89f0bf15278f81da2e17cfa0c19c58
sol <nathf1005@naver.com>
2024-09-03 23:15:29 +0900

    '내주변' 리스트 스타일 완료

--- src/components/MyLocation.js
+++ src/components/MyLocation.js
@@ -1,10 +1,17 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import getLocation from "../api/locationSearch"
 import { Map, MapMarker } from "react-kakao-maps-sdk"
+//Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLocationDot, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import noIMG from '../img/No_Image_Available.jpg'
 
 export default function MyLocation() {
+
+    const [list, setList] = useState([])
+
     useEffect(() => {
-        getLocation().then((data) => console.log('data', data.response.body.items.item))
+        getLocation().then((data) => setList([...data.response.body.items.item]))
     }, [])
 
     return (
@@ -13,6 +20,7 @@ export default function MyLocation() {
                 <div className="loc-list-search">
                     <div>
                         <input />
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
                     </div>
                     <div>
                         <ul>
@@ -22,23 +30,35 @@ export default function MyLocation() {
                             <li>숙박</li>
                         </ul>
                     </div>
-                    
+
                 </div>
                 <div className="loc-list-result">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <div className="list-img">
+                            <div><img src={noIMG} /></div>
+                        </div>
+                        <div className="list-item">
+                            <ul>
+                                <li>금강제화 명동본점</li>
+                                <li><span class="material-symbols-outlined">apartment</span>서울특별시 중구 명동8길 7</li>
+                                <li><span class="material-symbols-outlined">call</span>02-753-9411</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="loc-map">
                 <Map
-                    center={{ lat: 37.563446, lng: 126.983745 }}
-                    style={{ width: "100%", height: "100%" }}
+                    center={{ lat: 37.563446, lng: 126.983745 }}     //지도의 중심좌표
+                    style={{ width: "100%", height: "100%" }}        //지도 크기
+                    level={3}                                       //지도 확대 레벨
                 >
                     <MapMarker position={{ lat: 37.563446, lng: 126.983745 }}></MapMarker>
-                    <MapMarker position={{ lat: 37.5634380519, lng: 126.9847483173 }}></MapMarker>
+                    {
+                        list.map((v, i) => {
+                            <MapMarker position={{ lat: v.mapy, lng: v.mapx }}></MapMarker>
+                        })
+                    }
                 </Map>
             </div>
         </div>
