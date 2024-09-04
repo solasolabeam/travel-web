d770af325cb11e701de53d807e02dd1ff5259af1
sol <nathf1005@naver.com>
2024-09-04 16:25:35 +0900

    geLocation API 적용완료

--- src/components/MyLocation.js
+++ src/components/MyLocation.js
@@ -1,18 +1,67 @@
 import { useEffect, useState } from "react"
-import getLocation from "../api/locationSearch"
+// import getLocation from "../api/locationSearch"
 import { Map, MapMarker } from "react-kakao-maps-sdk"
 //Font Awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLocationDot, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
-import noIMG from '../img/No_Image_Available.jpg'
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import noIMG from '../img/No_Image_Available.jpg';
+import { key } from "../api/key";
 
 export default function MyLocation() {
-
     const [list, setList] = useState([])
+    const [lat, setLat] = useState(0)       //위도 Y좌표
+    const [lng, setLng] = useState(0)       //경도 X좌표
 
     useEffect(() => {
-        console.log('cc')
-        getLocation().then((data) => setList([...data.response.body.items.item]))
+        async function getLocation(pos) {
+            var url = 'https://apis.data.go.kr/B551011/KorService1/locationBasedList1';
+            var params = {
+                serviceKey: key,
+                numOfRows: '10',
+                pageNo: '1',
+                MobileOS: 'ETC',
+                MobileApp: 'AppTest',
+                arrange: 'A', //정렬구분
+                mapX: pos.coords.longitude,     //경도
+                mapY: pos.coords.latitude,     //위도
+                radius: '1500',   //거리반경
+                listYN: 'Y',   //목록구분
+
+            };
+
+            const queryString = new URLSearchParams(params).toString();  // url에 쓰기 적합한 querySting으로 return 해준다. 
+            const requrl = `${url}?${queryString}&_type=json`;
+
+            setLat(pos.coords.latitude)
+            setLng(pos.coords.longitude)
+
+            const res = await fetch(requrl)
+            const data = await res.json()
+
+            setList([...data.response.body.items.item])
+        }
+
+        function showErrorMsg(error) { // 실패했을때 실행
+            switch (error.code) {
+                case error.PERMISSION_DENIED: //Geolocation API의 사용 요청을 거부
+                    alert("위치 요청을 허용해 주세요!")
+                    break;
+
+                case error.POSITION_UNAVAILABLE:
+                    alert("가져온 위치 정보를 사용할 수 없습니다.")
+                    break;
+
+                case error.TIMEOUT:
+                    alert("위치 정보를 가져오기 위한 요청이 허용 시간을 초과했습니다.")
+                    break;
+
+                case error.UNKNOWN_ERROR:
+                    alert("알 수 없는 오류가 발생!")
+                    break;
+            }
+        }
+
+        navigator.geolocation.getCurrentPosition(getLocation, showErrorMsg);
     }, [])
 
     return (
@@ -45,11 +94,11 @@ export default function MyLocation() {
             </div>
             <div className="loc-map">
                 <Map
-                    center={{ lat: 37.563446, lng: 126.983745 }}     //지도의 중심좌표
+                    center={{ lat: lat, lng: lng }}     //지도의 중심좌표
                     style={{ width: "100%", height: "100%" }}        //지도 크기
-                    level={3}                                       //지도 확대 레벨
+                    level={5}                                       //지도 확대 레벨
                 >
-                    <MapMarker position={{ lat: 37.563446, lng: 126.983745 }}></MapMarker>
+                    <MapMarker position={{ lat: lat, lng: lng }}></MapMarker>
                     {
                         list.map((v, i) => {
                             return (
@@ -74,8 +123,8 @@ function Around({ value }) {
             <div className="list-item">
                 <ul>
                     <li>{value.title}</li>
-                    <li><span class="material-symbols-outlined">apartment</span>{value.addr1}</li>
-                    <li><span class="material-symbols-outlined">call</span>{value.tel}</li>
+                    <li><span className="material-symbols-outlined">apartment</span>{value.addr1}</li>
+                    <li><span className="material-symbols-outlined">call</span>{value.tel}</li>
                 </ul>
             </div>
         </div>
