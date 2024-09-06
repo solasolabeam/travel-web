1aa815ee47e8a08b89ecba061c162a2cf8ed5880
sol <nathf1005@naver.com>
2024-09-07 02:10:38 +0900

    swiper add

--- src/components/RecommendPart.js
+++ src/components/RecommendPart.js
@@ -1,23 +1,37 @@
-import { useSelector } from "react-redux"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 export default function RecommendPart() {
 
-  let sido = useSelector(state => state.sido)
 
   return (
-    <div className='local-container'>
-      <p>국내 지역별 여행지 추천</p>
-      <div className='local-griad-container'>
-        {
-          sido.map((v, i) => {
-            return (
-              <div key={v.code}>
-                <p>{v.name}</p>
-              </div>
-            )
-          })
-        }
-      </div>
-    </div>
+    <Swiper
+      slidesPerView={5}
+      spaceBetween={30}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="recommand-container"
+    >
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+      <SwiperSlide className='recommand-card-area'>Slide 1</SwiperSlide>
+    </Swiper>
   )
 }
