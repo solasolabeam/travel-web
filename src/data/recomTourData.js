089110f2a848df80fa9e7650b6b3ce65eec1863c
sol <nathf1005@naver.com>
2024-09-11 18:02:28 +0900

    수정

--- src/data/recomTourData.js
+++ src/data/recomTourData.js
@@ -1,37 +1,37 @@
 const recomTourData = [
     {
         title: '설악산',
-        addr: '강원특별자치도 양양군 서면 오색리',
+        addr: '강원특별자치도 양양군',
         fileName: 'tour1.jpg',
     },
     {
         title: '해운대',
-        addr: '부산광역시 해운대구 해운대해변로 264',
+        addr: '부산광역시 해운대구',
         fileName: 'tour2.jpg',
     },
     {
         title: '마장호수',
-        addr: '경기도 파주시 광탄면 기산로 365',
+        addr: '경기도 파주시',
         fileName: 'tour3.jpg',
     },
     {
         title: '용인 8경',
-        addr: '경기도 용인시 기흥구 동탄기흥로 923',
+        addr: '경기도 용인시',
         fileName: 'tour4.jpg',
     },
     {
         title: '암사생태공원',
-        addr: '서울특별시 강동구 선사로 83-66',
+        addr: '서울특별시 강동구',
         fileName: 'tour5.jpg',
     },
     {
         title: '원대리 자작나무 숲',
-        addr: '강원특별자치도 인제군 인제읍 자작나무숲길 760',
+        addr: '강원특별자치도 인제군',
         fileName: 'tour6.jpg',
     },
     {
         title: '연평도',
-        addr: '인천광역시 옹진군 연평면 연평중앙로24번길 3',
+        addr: '인천광역시 옹진군',
         fileName: 'tour7.jpg',
     },
 ]
